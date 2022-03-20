164ad074dcf30e79beb97dd68a35fdb3686c6504
Bikram Chatterjee <bikram@konnexions.ch>
2022-03-20 22:57:23 +0100

    beanstalk experiments

--- app.ts
+++ app.ts
@@ -4,6 +4,28 @@
  */
 
 require('./lib/startup/validateDependencies')().then(() => {
-  const server = require('./server')
-  server.start()
+  const cluster = require('cluster')
+  const app = require('./server')
+
+  const workers: any = {}
+  const count = require('os').cpus().length
+
+  function spawn () {
+    const worker = cluster.fork()
+    workers[worker.pid] = worker
+    return worker
+  }
+
+  if (cluster.isMaster) {
+    for (let i = 0; i < count; i++) {
+      spawn()
+    }
+    cluster.on('death', function (worker: any) {
+      console.log('worker ' + worker.pid + ' died. spawning a new process...')
+      delete workers[worker.pid]
+      spawn()
+    })
+  } else {
+    app.listen(process.env.PORT ?? 5000)
+  }
 })
